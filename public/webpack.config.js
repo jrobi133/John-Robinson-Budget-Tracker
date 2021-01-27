72eb1531817d5a596318d88d968db967894fe0b4
John Robinson <johnarobinson133@gmail.com>
2021-01-27 15:17:20 -0700

    added plugins for webpack.config and added service worker function to index.html page

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -2,11 +2,12 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 const config = {
-    entry: {
-
+    entry: { 
+      app: 'index.js',
     },
     output: {
-
+      path: __dirname + '/dist',
+      filename: '[name].bundle.js',
     },
     mode: "development",
     module: {
@@ -26,18 +27,18 @@ const config = {
     plugins: [
         new WebpackPwaManifest({
             fingerprints: false,
-            name: '',
-            short_name: '',
-            description: '',
+            name: 'Johns Budget Tracker',
+            short_name: 'Budget Tracker',
+            description: 'An application that allows you to keep track of expenses online or offline',
             background_color: '#01579b',
             theme_color: '#ffffff',
             'theme-color': '#ffffff',
             start_url: '/',
             icons: [
                 {
-                    src: path.resolve(''),
-                    sizes: [],
-                    destination: path.join ('',''),
+                    src: path.resolve('public/icons/icon-192x192.png'),
+                    sizes: [96, 128, 192, 256, 384, 512],
+                    destination: path.join ('public','icons'),
                 },
             ],
         }),
